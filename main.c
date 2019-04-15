f3d87bfa8cd180be655b00041139be926b88e5ca
Flynn <flynn@belmontharrison.net>
2019-04-15 18:46:59 +1000

    stage 2 started

--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 
 
 #include <stdio.h>
+#include <math.h>
 
 // Settings
-#define MAX_DELIVERYS 999
+#define MAX_DELIVERYS   999
+#define MAX_PAYLOAD     5.8 // Kg
+#define DRONE_MASS      3.4 // Kg
 
-#define STAGE_1 "S1"
+#define STAGE_1         "S1"
+#define STAGE_2         "S2"
+#define STAGE_3         "S3"
 
 typedef struct 
 {
@@ -20,7 +25,11 @@ typedef struct
     double mass;
 }delivery_t;
 
+// Prototypes 
+/* Prints the first and last elements of delivery_t array */
 void print_first_last(char *stage,delivery_t list[], int list_size);
+/* Sums up the masses in a delivery_t array */
+double delivery_t_mass_sum(delivery_t *list, int list_size);
 
 int main(int argc, char **argv)
 {
@@ -31,26 +40,35 @@ int main(int argc, char **argv)
     // Eat first line
     while(getchar() != '\n'){}
 
-    while((scanf("%lf %lf %lf", &x_buff, &y_buff, &m_buff) == 3) 
-            && lenght<MAX_DELIVERYS)
+    for(;(scanf("%lf %lf %lf", &x_buff, &y_buff, &m_buff) == 3) 
+            && lenght<MAX_DELIVERYS; lenght++)
     {
         deliverys[lenght].x = x_buff;
         deliverys[lenght].y = y_buff;
         deliverys[lenght].mass = m_buff;
-        lenght++;
     }
 
     print_first_last(STAGE_1, deliverys, lenght-1);
+    printf("%s, total to deliver: %3.2lf Kg\n",
+        STAGE_1, delivery_t_mass_sum(deliverys, lenght-1));
 }
 
 void print_first_last(char *stage, delivery_t *list, int list_size)
 {
     printf("%s, total data lines: %3d\n",stage, list_size);
     // First and last elements of array
-    printf("%s, first data line : x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
+    printf("%s, first data line :  x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
         stage, list[0].x, list[0].y, list[0].mass);
-    printf("%s, final data line : x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
+    printf("%s, final data line :  x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
         stage, list[list_size].x, list[list_size].y, list[list_size].mass);
-    
-    
+}
+
+double delivery_t_mass_sum(delivery_t *list, int list_size)
+{
+    double sum = 0;
+    for (int i = 0; i<list_size; i++)
+    {
+        sum += list[i].mass;
+    }
+    return sum;
 }
