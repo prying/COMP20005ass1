85bc4120eea6c89b75fb7599dbe192ba52c840b7
Flynn <flynn@belmontharrison.net>
2019-04-15 16:56:30 +1000

    finished stage 1

--- main.c
+++ main.c
@@ -31,7 +31,8 @@ int main(int argc, char **argv)
     // Eat first line
     while(getchar() != '\n'){}
 
-    while((scanf("%lf %lf %lf", &x_buff, &y_buff, &m_buff) == 3) && lenght<MAX_DELIVERYS)
+    while((scanf("%lf %lf %lf", &x_buff, &y_buff, &m_buff) == 3) 
+            && lenght<MAX_DELIVERYS)
     {
         deliverys[lenght].x = x_buff;
         deliverys[lenght].y = y_buff;
@@ -39,14 +40,17 @@ int main(int argc, char **argv)
         lenght++;
     }
 
-    print_first_last(STAGE_1,deliverys, lenght);
+    print_first_last(STAGE_1, deliverys, lenght-1);
 }
 
 void print_first_last(char *stage, delivery_t *list, int list_size)
 {
     printf("%s, total data lines: %3d\n",stage, list_size);
     // First and last elements of array
-    printf("%s, %lf, %lf, %lf\n",stage, list[0].x, list[0].y, list[0].mass);
-    printf("%s, %lf, %lf, %lf\n",stage, list[list_size].x, list[list_size].y, list[list_size].mass);
+    printf("%s, first data line : x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
+        stage, list[0].x, list[0].y, list[0].mass);
+    printf("%s, final data line : x=%4.1lf, y=%4.1lf, kg=%4.1lf\n",
+        stage, list[list_size].x, list[list_size].y, list[list_size].mass);
+    
     
 }
